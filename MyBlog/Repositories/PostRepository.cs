c4e062cfd0a7e5cb0d3ab18f1a2529c9e856cb3c
dannyliu2 <133186441+dannyliu2@users.noreply.github.com>
2023-09-25 18:22:49 +0800

    建立 PostRepository

---
+++ MyBlog/Repositories/PostRepository.cs
@@ -0,0 +1,11 @@
+﻿using MyBlog.Models;
+
+namespace MyBlog.Repositories
+{
+    public class PostRepository : Repository<Post>, IPostRepository
+    {
+        public PostRepository(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
+    }
+}
