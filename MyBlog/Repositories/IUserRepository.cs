f483af5baee9d79944577bddc9a5812035c1c37e
dannyliu2 <133186441+dannyliu2@users.noreply.github.com>
2023-09-26 09:32:07 +0800

    建立 UserRepository

---
+++ MyBlog/Repositories/IUserRepository.cs
@@ -0,0 +1,8 @@
+﻿using MyBlog.Models;
+
+namespace MyBlog.Repositories
+{
+    public interface IUserRepository : IRepository<User>
+    {
+    }
+}
---
+++ MyBlog/Repositories/UserRepository.cs
@@ -0,0 +1,11 @@
+﻿using MyBlog.Models;
+
+namespace MyBlog.Repositories
+{
+    public class UserRepository : Repository<User>, IUserRepository
+    {
+        public UserRepository(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
+    }
+}
