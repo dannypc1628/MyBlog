afd1dcae6f3bd8738bfa8fd632183eec91e8f11f
dannyLiu <h152074716@gmail.com>
2023-09-29 07:25:54 +0800

    新增文章功能

---
+++ MyBlog/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyBlog.Models;
+using MyBlog.Services;
+
+namespace MyBlog.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly IPostService _postService;
+
+        public AdminController(IPostService postService)
+        {
+            _postService = postService;
+        }
+        public IActionResult CreatePost()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreatePost(Post post)
+        {
+            _postService.CreateAsync(post);
+            return RedirectToAction("Index","Home");
+        }
+    }
+}
