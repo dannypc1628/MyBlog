92544329938f364625d23e836a99bd131fa08e6e
dannyLiu <h152074716@gmail.com>
2023-10-01 13:50:42 +0800

    建立 UpdatePost Action 與 cshtml

--- MyBlog/Controllers/AdminController.cs
+++ MyBlog/Controllers/AdminController.cs
@@ -23,5 +23,18 @@ namespace MyBlog.Controllers
             _postService.CreateAsync(post);
             return RedirectToAction("Index","Home");
         }
+
+        public async Task<IActionResult> UpdatePost(int id)
+        {
+            var post = await _postService.GetAsync(id);
+            return View(post);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdatePost(Post post)
+        {
+            await _postService.UpdateAsync(post);
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
