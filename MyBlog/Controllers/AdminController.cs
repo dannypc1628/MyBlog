77d51ab25f49a83ad1011f00cb5011f966f41250
dannyliu2 <133186441+dannyliu2@users.noreply.github.com>
2023-10-02 19:03:20 +0800

    建立圖片上傳功能

--- MyBlog/Controllers/AdminController.cs
+++ MyBlog/Controllers/AdminController.cs
@@ -7,10 +7,12 @@ namespace MyBlog.Controllers
     public class AdminController : Controller
     {
         private readonly IPostService _postService;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public AdminController(IPostService postService)
+        public AdminController(IPostService postService, IWebHostEnvironment webHostEnvironment)
         {
             _postService = postService;
+            _webHostEnvironment = webHostEnvironment;
         }
         public IActionResult CreatePost()
         {
@@ -21,7 +23,7 @@ namespace MyBlog.Controllers
         public IActionResult CreatePost(Post post)
         {
             _postService.CreateAsync(post);
-            return RedirectToAction("Index","Home");
+            return RedirectToAction("Index", "Home");
         }
 
         public async Task<IActionResult> UpdatePost(int id)
@@ -35,6 +37,38 @@ namespace MyBlog.Controllers
         {
             await _postService.UpdateAsync(post);
             return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult UploadImage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadImage(IFormFile image)
+        {
+            if (image != null && image.Length > 0)
+            {
+                var imagePath = Path.Combine(_webHostEnvironment.WebRootPath, "image");
+                if (!Directory.Exists(imagePath))
+                {
+                    Directory.CreateDirectory(imagePath);
+                }
+
+                var extenrsion = Path.GetExtension(image.FileName);
+                var fileName = $"{Guid.NewGuid()}{extenrsion}";
+
+                using (var stream = System.IO.File.Create($"{imagePath}/{fileName}"))
+                {
+                    await image.CopyToAsync(stream);
+                }
+
+                var webPath = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host}/image/{fileName}";
+
+                ModelState.AddModelError("", webPath);
+            }
+
+            return View();
         }
     }
 }
