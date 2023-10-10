a3fc1bbb8b57a387e559f65fcfecd18ab57cb5a6
dannyLiu <h152074716@gmail.com>
2023-10-10 16:15:42 +0800

    實作 sitemap.xml

--- MyBlog/Controllers/HomeController.cs
+++ MyBlog/Controllers/HomeController.cs
@@ -2,6 +2,10 @@
 using MyBlog.Models;
 using MyBlog.Services;
 using System.Diagnostics;
+using System.Globalization;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace MyBlog.Controllers
 {
@@ -15,7 +19,6 @@ namespace MyBlog.Controllers
             _logger = logger;
             _postService = postService;
         }
-
 
         public IActionResult Index(int p = 1)
         {
@@ -33,6 +36,42 @@ namespace MyBlog.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [Route("/sitemap.xml")]
+        public IActionResult SitemapXml()
+        {
+            var xmlDoc = new XDocument(
+                    new XDeclaration("1.0", "UTF-8", "yes")
+             );
+
+            XNamespace sitemap = "http://www.sitemaps.org/schemas/sitemap/0.9";
+            var urlset = new XElement(sitemap + "urlset");
+            // 這段不是必須
+            XNamespace xsi = "http://www.w3.org/2001/XMLSchema-instance";
+            XNamespace image = "http://www.google.com/schemas/sitemap-image/1.1";
+            XNamespace schemaLocation = "http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd http://www.google.com/schemas/sitemap-image/1.1 http://www.google.com/schemas/sitemap-image/1.1/sitemap-image.xsd";
+            urlset.Add(new XAttribute(XNamespace.Xmlns + "xsi", xsi));
+            urlset.Add(new XAttribute(XNamespace.Xmlns + "image", image));
+            urlset.Add(new XAttribute(xsi + "schemaLocation", schemaLocation));
+            // 這段不是必須 End
+
+            var posts = _postService.GetAll(1, int.MaxValue);
+
+            foreach (var post in posts)
+            {
+                var loc = $"{Request.Scheme}://{Request.Host}/{post.Path}";
+
+                var element = new XElement("url",
+                    new XElement("loc", loc),
+                    new XElement("lastmod", post.UpdateDate.ToString("yyyy-MM-ddThh:mmzzz", CultureInfo.InvariantCulture))
+                    );
+                urlset.Add(element);
+            }
+
+            xmlDoc.Add(urlset);
+
+            return Content(xmlDoc.ToString(), "application/xml");
         }
     }
 }
