2e56eda78c058a3bd5ffe8b5a8dcaa4d4f9ca5d9
dannyliu2 <133186441+dannyliu2@users.noreply.github.com>
2023-10-12 19:25:02 +0800

    建立註冊、登入、登出相關動作與檢視

--- MyBlog/Controllers/InitController.cs
+++ MyBlog/Controllers/InitController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyBlog.Models;
 using MyBlog.Services;
 
 namespace MyBlog.Controllers
@@ -6,10 +7,39 @@ namespace MyBlog.Controllers
     public class InitController : Controller
     {
         private readonly IImportService _importService;
+        private readonly IUserService _userService;
 
-        public InitController(IImportService importService)
+        public InitController(IImportService importService, IUserService userService)
         {
             _importService = importService;
+            _userService = userService;
+        }
+
+        public IActionResult CreateUser()
+        {
+            if (_userService.GetCount() > 0)
+            {
+                return RedirectToAction("Login", "Login");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(UserViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _userService.InitCreateAsync(viewModel);
+                if (result.IsSuccess)
+                {
+                    return RedirectToAction("Login", "Login");
+                }
+
+                ModelState.AddModelError(string.Empty, result.Messages.First());
+            }
+
+            return View(viewModel);
         }
 
         public IActionResult Import()
---
+++ MyBlog/Controllers/LoginController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using MyBlog.Models;
+using MyBlog.Services;
+
+namespace MyBlog.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly IUserService _userService;
+
+        public LoginController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _userService.LoginAsync(viewModel);
+
+                if (result.IsSuccess)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                ModelState.AddModelError("Email", result.Messages.First());
+            }
+
+            return View(viewModel);
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
